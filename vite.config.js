afbe0094c8da107a518bbffefe3265fdf149daad
BinElvan <147990726+BinElvan3126@users.noreply.github.com>
2026-06-14 22:01:07 +0700

    Change the base:

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/anime-warrior-3-odds/",
+  base: "/",
   plugins: [react()],
 });
